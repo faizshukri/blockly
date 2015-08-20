8f199b0a1f6498ec1d7f40345bdf35618e31158f
Faiz Shukri <faizshukri90@gmail.com>
2015-08-20 21:49:13 +0100

    Add some space in assert generated code

--- generators/java/assert.js
+++ generators/java/assert.js
@@ -9,7 +9,7 @@ Blockly.Java['assert_true'] = function(block) {
     var argument0 = Blockly.Java.valueToCode(block, 'VAR',
             Blockly.Java.ORDER_NONE) || '""';
 
-    var code = "assertTrue("+argument0+");\n";
+    var code = "assertTrue( "+argument0+" );\n";
     return code;
 };
 
@@ -17,7 +17,7 @@ Blockly.Java['assert_false'] = function(block) {
     var argument0 = Blockly.Java.valueToCode(block, 'VAR',
             Blockly.Java.ORDER_NONE) || '""';
 
-    var code = "assertFalse("+argument0+");\n";
+    var code = "assertFalse( "+argument0+" );\n";
     return code;
 };
 
@@ -28,14 +28,14 @@ Blockly.Java['assert_equals'] = function(block) {
     var argument1 = Blockly.Java.valueToCode(block, 'VAR2',
             Blockly.Java.ORDER_NONE) || '""';
 
-    var code = "assertEquals("+argument0+", "+argument1+");\n";
+    var code = "assertEquals( "+argument0+", "+argument1+" );\n";
     return code;
 };
 Blockly.Java['assert_null'] = function(block) {
     var argument0 = Blockly.Java.valueToCode(block, 'VAR',
             Blockly.Java.ORDER_NONE) || '""';
 
-    var code = "assertNull("+argument0+");\n";
+    var code = "assertNull( "+argument0+" );\n";
     return code;
 };
 
@@ -43,6 +43,6 @@ Blockly.Java['assert_notnull'] = function(block) {
     var argument0 = Blockly.Java.valueToCode(block, 'VAR',
             Blockly.Java.ORDER_NONE) || '""';
 
-    var code = "assertNotNull("+argument0+");\n";
+    var code = "assertNotNull( "+argument0+" );\n";
     return code;
 };
